88bc936d7e07d61619d7c13aa52658fd18af8f15
das-Sumyadeep <133164855+das-Sumyadeep@users.noreply.github.com>
2024-04-23 16:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function shortcut(s1, s2) {
-  // your code here
+// your code here
+	let str = s1[0] + s2[0];
+	
+	return str;
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
+
+
